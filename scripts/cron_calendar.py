0e204d8a5547d5f763438f19e3dc204be48ba8c7
JJ <jakesjordan00@gmail.com>
2026-05-14 15:26:32 -0400

    changing kustomer backfill to once an hour

---
+++ scripts/cron_calendar.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from pathlib import Path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+dir_path = Path(__file__).parent.parent
+with open(str(dir_path / 'function_app.py'), 'r') as r:
+    function_app = r.read()
+
+lines = function_app.split('\n')
+
+lines = [line.strip().replace(
+            '(timer: af.TimerRequest):', ''
+        ).replace(
+            'def ', ''
+        ).replace(
+            "schedule = '", ''
+        ).replace(
+            "',", ''
+        ).replace(
+            '",', ''
+        )
+for line in lines if 'schedule = ' in line or ('def' in line and '(timer: af.TimerRequest)' in line)]
+bp = 'here'
+
+
+
+schedule = {}
+
+for i, line in enumerate(lines):
+    if '*' in line: continue
+    schedule[line] = lines[i-1]
+
+bp = 'here'
