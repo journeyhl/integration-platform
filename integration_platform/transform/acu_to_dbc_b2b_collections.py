f659db2e5b373666dc603465a7df4f1ed282b9f6
JJ <jakesjordan00@gmail.com>
2026-07-20 16:13:35 -0400

    bumping redstag to go at 07 and 37 instead of 05 and 35

--- integration_platform/transform/acu_to_dbc_b2b_collections.py
+++ integration_platform/transform/acu_to_dbc_b2b_collections.py
@@ -18,8 +18,27 @@ class Transform:
         #.with_columns(pl.lit(datetime.now(ZoneInfo('America/New_York')), pl.Datetime).alias('LastChecked'))
         bp = 'here'
         data_extract = data_extract.with_columns(pl.lit(datetime.now(ZoneInfo('America/New_York')), pl.Datetime).alias('LastChecked'))
-        
+        self.data_extract = data_extract
+        collections_summary, collections_summary_snapshot = self._get_summary_data_()
+        balance_by_status = self._aggregate_by_metric_(group_by_columns=['CustomerStatus'])
+        balance_by_salesrep = self._aggregate_by_metric_(group_by_columns=['SalespersonID'])
+        # df_transformed = data_extract.group_by(group_by_columns, maintain_order=True).agg(
+        #     pl.col('VendorPartNumber').drop_nulls().unique(maintain_order=True).str.join(', ')
+        # )
+        # self.pipeline.centralstore.__dataframe_to_table_create_statement__(data_extract)
+        # self.pipeline.centralstore.__dataframe_to_table_create_statement__(collections_summary)
+        data_transformed = {
+            'detail': data_extract,
+            'summary': collections_summary,
+            'summary_snapshot': collections_summary_snapshot,
+            'balance_by_status': balance_by_status,
+            'balance_by_salesrep': balance_by_salesrep,
+        }
+        return data_transformed
 
+
+
+    def _get_summary_data_(self):
         group_by_columns = [
             'CustomerID', 
             'CustomerName',
@@ -31,7 +50,7 @@ class Transform:
             'Phone',
             'LastChecked'
         ]
-        collections_summary = data_extract.group_by(group_by_columns, maintain_order=True).agg(
+        collections_summary = self.data_extract.group_by(group_by_columns, maintain_order=True).agg(
             pl.col('CurrentBalance').sum(), 
             pl.col('Balance_1_30d').sum(),
             pl.col('Balance_31_60d').sum(),
@@ -40,15 +59,16 @@ class Transform:
             pl.col('TotalBalance').sum()
         )
         collections_summary_snapshot = collections_summary.with_columns(pl.col('LastChecked').alias('Timestamp')).drop('LastChecked')
-        bp = 'here'
-        # df_transformed = data_extract.group_by(group_by_columns, maintain_order=True).agg(
-        #     pl.col('VendorPartNumber').drop_nulls().unique(maintain_order=True).str.join(', ')
-        # )
-        # self.pipeline.centralstore.__dataframe_to_table_create_statement__(data_extract)
-        # self.pipeline.centralstore.__dataframe_to_table_create_statement__(collections_summary)
-        data_transformed = {
-            'detail': data_extract,
-            'summary': collections_summary,
-            'summary_snapshot': collections_summary_snapshot
-        }
-        return data_transformed
+        return collections_summary, collections_summary_snapshot
+    
+    def _aggregate_by_metric_(self, group_by_columns: list = []):
+        aggregate_data = self.data_extract.group_by(group_by_columns, maintain_order=True).agg(
+            pl.col('CurrentBalance').sum(), 
+            pl.col('Balance_1_30d').sum(),
+            pl.col('Balance_31_60d').sum(),
+            pl.col('Balance_61_90d').sum(),
+            pl.col('Balance_90d').sum(),
+            pl.col('TotalBalance').sum()
+
+        ).with_columns(pl.lit(datetime.now(ZoneInfo('America/New_York')), pl.Datetime).alias('LastChecked'))
+        return aggregate_data
