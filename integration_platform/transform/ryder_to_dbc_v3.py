ac597ae30d85ed57beba55b5020856e1cf3ede4a
JJ <jakesjordan00@gmail.com>
2026-08-10 18:33:44 -0400

    ryder to dbc updates and further progress, plus updating documentation standards

--- integration_platform/transform/ryder_to_dbc_v3.py
+++ integration_platform/transform/ryder_to_dbc_v3.py
@@ -36,25 +36,58 @@ class Transform:
     def _parse_order_info_(self, order_info: dict, sender: Literal['history', 'milestone']):
         '''We can expect  account, brand, consigneeInfo, serviceLocation, shipperInfo to not be in milestone'''
         tracking_nbr = order_info['lobTracking']
+        self.tracking_nbr = tracking_nbr
         rlm_order_nbr= order_info['orderNumber']
         references = order_info['orderReferences']
         current_status = order_info['currentOrderStatus']
         events = order_info['mileStoneEvents']
         #history only
-        consignee_info = order_info.get('consigneeInfo')
-        service_location = order_info.get('serviceLocation')
-        shipper_info = order_info.get('shipperInfo')
+        consignee_info = order_info.get('consigneeInfo') or {}
+        service_location = order_info.get('serviceLocation') or {}
+        shipper_info = order_info.get('shipperInfo') or {}
 
 
         fmt_references = self.__parse_references__(references=references)
         fmt_current_status = self.__parse_event__(event=current_status, event_nbr=current_status['sequence'], refs=fmt_references)
         fmt_events = [self.__parse_event__(event=event, event_nbr=i+1, refs=fmt_references) for i, event in enumerate(events)]
-
+        fmt_cosignee = self._parse_cosignee_info_(cosignee=consignee_info)
+        fmt_service_location = self.__parse_service_location__(service_location=service_location)
+        fmt_shipper = self.__parse_shipper_info__(shipper_info=shipper_info)
 
         bp = 'here'
 
 
+
+
+        
+
     def __parse_references__(self, references: list[dict]) -> dict:
+        ''':class:`~Transform`.:meth:`~__parse_references__` (self, references: *list[dict]*):
+        ---
+        <hr>
+        
+        Parses the references list from Ryder API, which contains our key values
+        
+        ### Downstream Calls 
+         #### :class:`~integration_platform.transform.ryder_to_dbc_v3.Transform`.:meth:`~integration_platform.transform.ryder_to_dbc_v3.Transform.___get_order_nbr_from_reference___`
+            - Gets `Ordertype` and `OrderNbr` when referenceType == 'PO'
+        
+        ### Upstream Calls 
+         #### :class:`~integration_platform.transform.ryder_to_dbc_v3.Transform`.:meth:`~integration_platform.transform.ryder_to_dbc_v3.Transform._parse_order_info_`
+            - Description
+            
+        <hr>
+        
+        Parameters
+        ---
+        :param (*list[dict]*) `references`: list of dicts, each dict containing info for a different reference/key value
+        
+        <hr>
+        
+        Returns
+        ---
+        :return `refs` (dict): Reference numbers in dictionary format. We should get `ShipmentNbr`, `RyderID`, `OrderNbr` and `OrderType`
+        '''        
         refs = {}
         for ref in references:
             if ref['referenceType'].lower() in['client', 'oo']:
@@ -66,7 +99,34 @@ class Transform:
         return refs
 
     def __parse_event__(self, event: dict, event_nbr: int, refs: dict):
-        fmt_current_status = {
+        ''':class:`~Transform`.:meth:`~__parse_event__` (self, event: *dict*, event_nbr: *int*, refs: *dict*, ):
+        ---
+        <hr>
+        
+        Given a milestone event OR the current status, clean up and format for SQL
+        
+        ### Downstream Calls 
+         #### :class:`~integration_platform.transform.default_transformer.DefaultTransformer`.:meth:`~integration_platform.transform.default_transformer.DefaultTransformer.parse_date_str`
+            - Parses dates
+        
+        ### Upstream Calls 
+         #### :class:`~integration_platform.transform.ryder_to_dbc_v3.Transform`.:meth:`~integration_platform.transform.ryder_to_dbc_v3.Transform._parse_order_info_` x2
+
+        <hr>
+        
+        Parameters
+        ---
+        :param (*dict*) `event`: dictionary of even data
+        :param (*int*) `event_nbr`: The event number or sequence. 1 is frst, every other event incremenmts by 1
+        :param (*dict*) `refs`: Formatted reference number dict
+        
+        <hr>
+        
+        Returns
+        ---
+        :return `fmt_event` (*dict*): Formatted dictionary of event/current status data.
+        '''        
+        fmt_event = {
             'ShipmentNbr': refs['ShipmentNbr'],
             'RyderID': refs['RyderID'],
             'EventNbr': event_nbr,
@@ -78,7 +138,7 @@ class Transform:
             'DatetimeLocal': self.pipeline.default_transformer.parse_date_str(date_str=event['dateTime'], tries=0, log_prefix=self.log_prefix),
             'DatetimeUTC': self.pipeline.default_transformer.parse_date_str(date_str=event['localDateTime'], tries=0, offset=True, log_prefix=self.log_prefix),
         }
-        return fmt_current_status
+        return fmt_event
 
 
 
@@ -123,6 +183,115 @@ class Transform:
 
 
 
+
+
+    def _parse_cosignee_info_(self, cosignee: dict):
+        ''':class:`~Transform`.:meth:`~_parse_cosignee_` (self, cosignee: *dict*, ):
+        ---
+        <hr>
+        
+        Given a dictionary of cosignee info (Ship-To name, address, phones and email), format for SQL
+        
+        ### Downstream Calls 
+         #### :class:`~integration_platform.transform.default_transformer.DefaultTransformer`.:meth:`~integration_platform.transform.default_transformer.DefaultTransformer.clean_string`
+            - Description
+         #### :class:`~integration_platform.transform.default_transformer.DefaultTransformer`.:meth:`~integration_platform.transform.default_transformer.DefaultTransformer.parse_phone`
+            - Description
+        
+        ### Upstream Calls 
+         #### :class:`~integration_platform.transform.ryder_to_dbc_v3.Transform`.:meth:`~integration_platform.transform.ryder_to_dbc_v3.Transform._parse_order_info_`
+            
+        <hr>
+        
+        Parameters
+        ---
+        :param (*dict*) `cosignee`: dictionary of ship to info
+        
+        <hr>
+        
+        Returns
+        ---
+        :return `fmt_cosignee` (_dict_): Dictionary with cleaned up ship-to data  
+        '''        
+        bp = 'here'
+        if cosignee == {}:
+            self.logger.info(f'{self.log_prefix}No cosignee, returning None...')
+            return None
+        fmt_cosignee = {
+            'Name': cosignee['name'],
+            'Address1': self.pipeline.default_transformer.clean_string(string=cosignee['address1']),
+            'Address2': self.pipeline.default_transformer.clean_string(string=cosignee['address2']),
+            'Address3': self.pipeline.default_transformer.clean_string(string=cosignee['address3']),
+            'Address4': self.pipeline.default_transformer.clean_string(string=cosignee['address4']),
+            'City': cosignee['city'],
+            'State': cosignee['state'],
+            'Zip': cosignee['zip'],
+            'Email': cosignee['email'] if cosignee['email'] != '' else None,
+            'PhonePrimary': self.pipeline.default_transformer.parse_phone(phone_str=cosignee['primaryContact'], log_prefix=self.log_prefix),
+            'PhoneCell': self.pipeline.default_transformer.parse_phone(phone_str=cosignee['cell'], log_prefix=self.log_prefix),
+            'PhoneAlt': self.pipeline.default_transformer.parse_phone(phone_str=cosignee['alternatePhone'], log_prefix=self.log_prefix),
+            'PhoneOffice': self.pipeline.default_transformer.parse_phone(phone_str=cosignee['officePhone'], log_prefix=self.log_prefix),
+            'PhoneExt': self.pipeline.default_transformer.parse_phone(phone_str=cosignee['extensionPhone'], log_prefix=self.log_prefix),
+            # 'AltPhone': cosignee['alternatePhone'] if cosignee['alternatePhone'] != '' else None,
+        }
+        return fmt_cosignee
+
+
+
+
+
+
+
+    def __parse_emails__(self, emails: str | None):
+        emails = self.pipeline.default_transformer._handle_none_and_empty_strings_(string=emails, string_descr='Emails', log_prefix=self.log_prefix)
+        if emails == None:
+            return
+        if ' ' not in emails.strip():
+            return emails.strip().lower()
+        spl_email = emails.strip().split(' ')
+        self.logger.warning(f'{len(spl_email)} email addresses found!')
+        return ';'.join([e.lower() for e in spl_email])
+        
+
+
+    def __parse_service_location__(self, service_location: dict):
+        if service_location == {}:
+            self.logger.info(f'No Service Location provided, returning None...')
+            return None
+        emails = self.__parse_emails__(emails=service_location['email'])
+        fmt_service_location = {
+            'LocationType': self.pipeline.default_transformer.clean_string(string=service_location['locationType'], log_prefix=self.log_prefix),
+            'HubCode': self.pipeline.default_transformer.clean_string(string=service_location['hubCode'], log_prefix=self.log_prefix),
+            'Name': self.pipeline.default_transformer.clean_string(string=service_location['name'], log_prefix=self.log_prefix),
+            'Address1': self.pipeline.default_transformer.clean_string(string=service_location['address1'], log_prefix=self.log_prefix),
+            'Address2': self.pipeline.default_transformer.clean_string(string=service_location['address2'], log_prefix=self.log_prefix),
+            'City':  self.pipeline.default_transformer.string_case_pascal(string=service_location['city'], log_prefix=self.log_prefix),
+            'State': self.pipeline.default_transformer.clean_string(string=service_location['state'], log_prefix=self.log_prefix),
+            'Zip': self.pipeline.default_transformer.clean_string(string=service_location['zip'], log_prefix=self.log_prefix),
+            'Phone': self.pipeline.default_transformer.clean_string(string=service_location['phone1'], log_prefix=self.log_prefix),
+            'Phone2': self.pipeline.default_transformer.clean_string(string=service_location['phone2'], log_prefix=self.log_prefix),
+            'Phone3': self.pipeline.default_transformer.clean_string(string=service_location['phone3'], log_prefix=self.log_prefix),
+            'Email': self.__parse_emails__(emails=emails),            
+        }
+        bp = 'here'
+        return fmt_service_location
+        
+    def __parse_shipper_info__(self, shipper_info: dict):
+        if shipper_info == {}:
+            self.logger.info(f'No Shipper Info provided, returning None...')
+            return None
+        fmt_shipper_info = {
+            'Name': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperName'], log_prefix=self.log_prefix),
+            'Address1': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperAddress1'], log_prefix=self.log_prefix),
+            'Address2': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperAddress2'], log_prefix=self.log_prefix),
+            'Address3': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperAddress3'], log_prefix=self.log_prefix),
+            'Address4': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperAddress4'], log_prefix=self.log_prefix),
+            'City':  self.pipeline.default_transformer.string_case_pascal(string=shipper_info['shipperCity'], log_prefix=self.log_prefix),
+            'State': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperState'], log_prefix=self.log_prefix),
+            'Zip': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperZip'], log_prefix=self.log_prefix),
+            'Phone': self.pipeline.default_transformer.clean_string(string=shipper_info['shipperPhone'], log_prefix=self.log_prefix),         
+        }
+        return fmt_shipper_info
 
 
     def _compare_diff_(self):
