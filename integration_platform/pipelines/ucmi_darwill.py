d461518dd63ee486fd1a139f50685d309e1b7022
JJw <jakesjordan00@gmail.com>
2026-08-25 12:34:27 -0400

    adding in ucmi pipelines, just structure for now...need to fill in from other repo

---
+++ integration_platform/pipelines/ucmi_darwill.py
@@ -0,0 +1,24 @@
+from integration_platform.pipelines import Pipeline
+
+
+
+class UCMI_DarwillCustomers(Pipeline):
+    def __init__(self, function: str, env: str='prod'):
+        super().__init__(pipeline_name='ucmi-darwill-customers', function=function, env=env)
+        # self.hubspot = HubSpotAPI(self)
+
+    def extract(self):
+        data_extract = {}
+        return data_extract
+
+    def transform(self, data_extract):
+        data_transformed = {}
+       
+        return data_transformed
+    
+    def load(self, data_transformed):
+        data_loaded = {}
+        return data_loaded
+    
+    def log_results(self, data_loaded):
+        pass
